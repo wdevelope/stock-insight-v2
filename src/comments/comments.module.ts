105baf03e8f2d191b864b4bc165b537b4323b3fd
Lee seongwon <jsw6357@naver.com>
2023-08-17 10:39:06 +0900

    fix:commnet module 코드 수정

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -3,9 +3,18 @@ import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Comment } from './entities/comment.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { config } from 'dotenv';
+config();
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment])],
+  imports: [
+    TypeOrmModule.forFeature([Comment]),
+    JwtModule.register({
+      secret: process.env.DATABASE_SECRET_KEY,
+      signOptions: { expiresIn: process.env.DATABASE_EXPIRESIN },
+    }),
+  ],
   exports: [TypeOrmModule],
   controllers: [CommentsController],
   providers: [CommentsService],
