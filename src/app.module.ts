92c03691d5e54896d38b7797069a3102a02d5fe6
Lee seongwon <jsw6357@naver.com>
2023-08-17 10:03:44 +0900

    fix:app.module 코드 수정 및 데이터베이스 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,31 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import { Users } from './users/users.entity';
 import { BoardsModule } from './boards/boards.module';
 import { CommentsModule } from './comments/comments.module';
+import { TypeOrmConfig } from 'config/typeorm.config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: [Users],
-      synchronize: true,
-    }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: './.env' }),
+    TypeOrmModule.forRoot(TypeOrmConfig),
     UsersModule,
     BoardsModule,
     CommentsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
