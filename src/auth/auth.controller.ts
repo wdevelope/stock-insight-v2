80cad8b8513de51010fb1ea79ef4235f3709f60e
seongwong woo <dnqkr18@gmail.com>
2023-08-22 19:48:56 +0900

    kakao

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,24 +1,28 @@
-import { Body, Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, UseGuards, Req, Res } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { UsersService } from '../users/users.service';
-import { Request, Response } from 'express';
-import { KakaoLoginAuthDto } from './dto/kakao.dto';
+import { KakaoAuthGuard } from './auth.guard';
+// import { AuthGuard } from '@nestjs/passport';
 
-@Controller('kakao')
+@Controller('api/kakao')
 export class AuthController {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly usersService: UsersService,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Get('/login')
-  @UseGuards(AuthGuard('kakao')) //인증과정을 거쳐야하기때문에 UseGuards를 써주고 passport인증으로 AuthGuard를 써준다. 이름은 kakao로
-  async loginKakao(
-    @Body() body: KakaoLoginAuthDto,
-    @Res() res: Response, //Nest.js가 express를 기반으로 하기때문에 Request는 express에서 import한다.
-  ) {
-    //프로필을 받아온 다음, 로그인 처리해야하는 곳(auth.service.ts에서 선언해준다)
-    return this.authService.OAuthLogin(body, res);
+  @UseGuards(KakaoAuthGuard)
+  async loginKakao() {
+    console.log('카카오 로그인 신청');
+  }
+
+  @Get('/login/callback')
+  @UseGuards(KakaoAuthGuard)
+  async callback(@Req() req, @Res() res) {
+    const user = req.user;
+    const token = await this.authService.generateJWT(user);
+    res.cookie('Authorization', `Bearer ${token}`, {
+      // httpOnly: true,
+      // secure: true, // HTTPS 사용 시 활성화
+      // maxAge: 1000 * 60 * 60 * 24 * 7, // 쿠키 유효 기간 설정 (예: 1주일)
+    });
+    return res.redirect(`http://localhost:3000/view/index.html`);
   }
 }
