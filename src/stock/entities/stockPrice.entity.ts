05e4b31e05a0e784196744aa46e0478fc9ae57fe
Yeonbeom <dusqja14@naver.com>
2023-08-25 15:17:18 +0900

    stock entity 컬럼 수정

--- src/stock/entities/stockPrice.entity.ts
+++ src/stock/entities/stockPrice.entity.ts
@@ -65,6 +65,13 @@ export class StockPrice {
   @Column()
   whol_loan_rmnd_rate: string; // 융자 잔고 비율
 
+  @Column()
+  bstp_kor_isnm: string; // 업종명
+
+  @Column()
+  iscd_stat_cls_code: string; //종목 상태 구분 코드
+  //00(그외),51(관리종목),52(투자의견),53(투자경고),54(투자주의),55(신용가능),57(증거금 100%),58(거래정지),59(단기과열)
+
   @CreateDateColumn()
   created_at: Date;
 
