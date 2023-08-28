47e9f16573475340903453e87f9c34070087199d
Yeonbeom <dusqja14@naver.com>
2023-08-28 10:34:03 +0900

    stock scheduling 적용

--- src/stock/stock.controller.ts
+++ src/stock/stock.controller.ts
@@ -5,27 +5,50 @@ import { StockService } from './stock.service';
 export class StockController {
   constructor(private readonly stockService: StockService) {}
 
-  @Get('pricesave')
+  @Post('namesave')
   @HttpCode(200)
-  fetchAndSaveData(): string {
-    this.stockService.fetchDataAndSaveToDB();
-    return '데이터 저장 성공!';
+  stockNameSave(): string {
+    this.stockService.stockNameSave();
+    return '주식 목록 저장 성공!';
+  }
+
+  @Post('namesavestart')
+  @HttpCode(200)
+  startStockNameSave(): string {
+    this.stockService.startStockNameSave();
+    return '스케쥴 시작!';
+  }
+
+  @Post('namesavestop')
+  @HttpCode(200)
+  stopStockNameSave(): string {
+    this.stockService.stopStockNameSave();
+    return '스케쥴 종료!';
+  }
+
+  @Post('pricesave')
+  @HttpCode(200)
+  stockPriceSave(): string {
+    this.stockService.stockPriceSave();
+    return '주식 가격 저장 성공!';
+  }
+
+  @Post('pricesavestart')
+  @HttpCode(200)
+  startStockPriceSave(): string {
+    this.stockService.startStockPriceSave();
+    return '스케쥴 시작!';
+  }
+
+  @Post('pricesavestop')
+  @HttpCode(200)
+  stopStockPriceSave(): string {
+    this.stockService.stopStockPriceSave();
+    return '스케쥴 종료!';
   }
 
   @Get('prices')
   async getStockPrices() {
     return this.stockService.getStockPrices();
-  }
-
-  @Get('namesave')
-  @HttpCode(200)
-  stockNameSave(): string {
-    this.stockService.stockNameSave();
-    return '데이터 저장 성공!';
-  }
-
-  @Post('token')
-  async tokenCreate() {
-    return this.stockService.tokenCreate();
   }
 }
