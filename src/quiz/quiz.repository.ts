1e9171b3d4eb27799999f3d51153479ee602e1d9
SG <gwagbyeol@gmail.com>
2023-08-30 16:39:48 +0900

    database 수정 및 퀴즈 구현

--- src/quiz/quiz.repository.ts
+++ src/quiz/quiz.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Quiz } from './quiz.entity';
 import { DataSource, Repository } from 'typeorm';
 import { CreateQuizDto } from './dto/create-quiz.dto';
+import { UpdateQuizDto } from './dto/update-quiz.dto';
 import { Users } from 'src/users/users.entity';
 import { Stock } from 'src/stock/entities/stock.entity';
 
@@ -15,7 +16,7 @@ export class QuizRepository extends Repository<Quiz> {
     const quizId = await this.findOne({ where: { id } });
     return quizId;
   }
-
+  // 정답 제출
   async createQuiz(
     // stock: Stock,
     user: Users,
@@ -26,5 +27,11 @@ export class QuizRepository extends Repository<Quiz> {
       stockName: data.stockName,
       user,
     });
+  }
+
+  //정답 확인
+  async updateQuiz(quiz: Quiz, data: UpdateQuizDto): Promise<object> {
+    const result = await this.update({ id: quiz.id }, data);
+    return result;
   }
 }
