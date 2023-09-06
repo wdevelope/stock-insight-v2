541e39282c50b4f1adb3c8eb084b365790a435bd
Lee seongwon <jsw6357@naver.com>
2023-09-06 22:06:54 +0900

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,6 @@ TIME_NOW=$(date +%c)
 cd $PROJECT_ROOT
 
 pm2 delete $APP_NAME
-pm2 start npm --name $APP_NAME -- start
+pm2 start npm --name $APP_NAME -- start:prod
 
 echo "$TIME_NOW > Deploy has been completed"
